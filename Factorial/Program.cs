afc32c7cc7cc12a0218dc0417bc2c480e35cb9f7
jdavidg1 <jdavidg_1@hotmail.com>
2019-06-22 18:54:34 -0500

    Cálculo del factorial de un número

---
+++ Factorial/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Programa para calcular el factorial de un número
+
+            int n=0,numero = 0, resultado=1;
+
+            Console.Write("Cual es el número a calcular el factorial: ");
+
+            numero = Convert.ToInt32(Console.ReadLine());
+            
+
+            for(n=numero; n>=1; n--)
+            {
+                
+                resultado *= n;
+                
+            }
+            
+            Console.WriteLine($"El factorial de {numero} es {resultado}");
+            Console.ReadKey();
+                
+                
+        }
+    }
+}
